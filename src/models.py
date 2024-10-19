24399563cc71342485705633693d2d7b4abb74b8
Pirela47 <luis.jpl294@gmail.com>
2024-10-19 02:35:23 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -7,28 +7,42 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Follower (Base):
+    __tablename__ = "Follower"
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_from_id = Column(Integer,ForeignKey("User.id"))
+    user_to_id = Column(Integer,ForeignKey("User.id"))
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer, primary_key=True, unique=True)
+    user_name = Column(String(250))
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250), unique=True)
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'Post'
+    id = Column(Integer, primary_key=True, unique=True)
+    user_id = Column(Integer, ForeignKey('User.id'))
 
-## Draw from SQLAlchemy base
+class Comment(Base):
+    __tablename__ = 'Comment'
+    id = Column(Integer, primary_key=True, unique=True)
+    comment_text= Column(String(250))
+    author_id = Column(Integer, ForeignKey('User.id'))
+    post_id = Column(Integer, ForeignKey('Post.id'))
+
+
+class Media(Base):
+    __tablename__ = 'Media'
+    id = Column(Integer, primary_key=True, unique=True)
+    type = Column(String(250))
+    url = Column(String(250))
+    post_id= Column(Integer, ForeignKey('Post.id'))
+
+
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
